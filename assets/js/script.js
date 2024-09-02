31906eccee7149a7bdeaf40994408f20b5ac40ac
IRCF <ircfspace@gmail.com>
2024-09-02 20:12:58 +0330

    v1.4

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,14 +8,14 @@ $(document).on('click', '#copyFromQR, #copyUrl', function (e) {
     alert('آدرس در کلیپ‌بورد کپی شد.');
 });
 
-let source = 'yebekhe/TVC';
+let source = 'itsyebekhe/HiN-VPN';
 $(document).on('click', 'a[data-loc]', function (e) {
     e.preventDefault();
     let location = $(this).data('loc');
     let title = location.toUpperCase();
     $('#countryLoc a').removeClass('active');
     $(this).addClass('active');
-    let config = 'https://raw.githubusercontent.com/'+source+'/main/subscriptions/location/normal/'+title;
+    let config = 'https://raw.githubusercontent.com/'+source+'/main/subscription/location/normal/'+title;
     $('#qrcode img').attr('src', "https://quickchart.io/qr/?size=300x200&light=ffffff&text="+encodeURIComponent(config));
     $('#qrModal h4').html('QRCode ('+title+')');
     $('#qrcode input').val(config);
@@ -26,13 +26,19 @@ $('#qrModal').on('hidden.bs.modal', function () {
     $('#countryLoc a').removeClass('active');
 });
 
+function flagToCountryCode(flag) {
+    const codePoints = flag.split(' ');
+    return codePoints[1].toLowerCase();
+}
+
 function renderLocationData(locationPaths) {
     let html = '';
     locationPaths.forEach(function(element) {
+        let countryCode = flagToCountryCode(element);
         html += '<a href="" data-loc="'+element.toLowerCase()+'">';
         html += '<div class="slide">';
-        html += '<img src="./assets/img/flags/'+element.toLowerCase()+'.svg?v1.1" alt="'+element+'" />';
-        html += '<p dir="auto">'+element+'</p>';
+        html += '<img src="./assets/img/flags/'+countryCode+'.svg?v1.1" alt="'+element+'" />';
+        html += '<p dir="auto">'+countryCode.toUpperCase()+'</p>';
         html += '</div>';
         html += '</a>';
     });
@@ -45,7 +51,7 @@ window.addEventListener('load', function() {
     if (cachedData !== "undefined" && cachedTime !== "undefined" && (Date.now() - cachedTime < 15 * 60 * 1000)) {
         renderLocationData(cachedData.split(','));
     } else {
-        fetch('https://api.github.com/repos/yebekhe/TVC/contents/subscriptions/location/normal')
+        fetch('https://api.github.com/repos/'+source+'/contents/subscription/location/normal')
             .then(response => {
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
@@ -53,14 +59,14 @@ window.addEventListener('load', function() {
                 return response.json();
             })
             .then(data => {
-                let locationPaths = data.filter(item => !item.path.includes("XX") && !item.path.includes("CF"));
+                let locationPaths = data.filter(item => !item.path.includes("🇽🇽 XX") && !item.path.includes("🆥🆥"));
                 locationPaths = locationPaths.map(item => item.name);
                 localStorage.setItem('locationData', locationPaths);
                 localStorage.setItem('locationDataTime', Date.now());
                 renderLocationData(locationPaths);
             })
             .catch(error => {
-                renderLocationData(["IR", "TR", "GB", "US", "PL", "FI", "SE", "DE", "FR"]);
+                renderLocationData(["🇮🇷 IR", "🇹🇷 TR", "🇬🇧 GB", "🇺🇸 US", "🇵🇱 PL", "🇫🇮 FI", "🇸🇪 SE", "🇩🇪 DE", "🇫🇷 FR"]);
             });
     }
 });
